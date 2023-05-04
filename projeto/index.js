ed79e95efa12611c380cd153d1d0fe6f1116a423
RafaelAntoniete <rafaelantoniete0@gmail.com>
2023-05-04 18:09:21 -0300

    "atualizado"

--- projeto/index.js
+++
@@ -1,7 +0,0 @@
-const input = document.querySelector('#color')
-const btn = document.querySelector('#btn')
-const tatoo = document.querySelector('#backgound')
-
-btn.addEventListener('click', ()=> {
-    tatoo.style.fill = input.value;
-})
